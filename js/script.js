f576a5088399713f3c93148544ae0d6768f5e624
sahrul-dwann <sahruld18@gmail.com>
2025-04-13 01:02:00 +0700

    membuat website House

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btnHamburger = document.querySelector(".hamburger");
+
+const menu = document.querySelector(".menu")
+
+btnHamburger.addEventListener("click", () => {
+  btnHamburger.classList.toggle("is-active");
+  menu.classList.toggle("menu-active");
+});
